5f5f437cfd377515096802ad405712d6f100cbc5
Ian King <ian.antking@gmail.com>
2021-01-26 13:39:28 +0000

    no need to parse event.body

--- message-lambda/index.js
+++ message-lambda/index.js
@@ -1,14 +1,12 @@
 const fetch = require('node-fetch')
 
 exports.handler = async (event, context) => {
-    const eventBody = JSON.parse(event.body)
     const response = await fetch(process.env.SLACK_MESSAGE_ENDPOINT, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(eventBody)
+        body: event.body
     })
-    const responseBody = await response.json()
-    context.succeed(responseBody);
+    context.succeed(response);
 }
