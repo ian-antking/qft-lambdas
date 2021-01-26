f1d6f2aee7f0b793ac474aaa539060666a1461fc
Ian King <ian.antking@gmail.com>
2021-01-26 01:02:54 +0000

    send request body to slack

--- message-lambda/index.js
+++ message-lambda/index.js
@@ -1,3 +1,10 @@
+const fetch = require('node-fetch')
+
 exports.handler = async (event, context) => {
-    context.succeed('Hello World!');
+    const response = await fetch(process.env.SLACK_ENDPOINT, {
+        method: 'POST',
+        body: event.body
+    })
+    const body = await response.json()
+    context.succeed(JSON.stringify(body));
 }
