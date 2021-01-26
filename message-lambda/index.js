69ddbd346e3c415b6043e5d5c2a3dcf2435295e5
Ian King <ian.antking@gmail.com>
2021-01-26 22:10:39 +0000

    events from api gateway are the body

--- message-lambda/index.js
+++ message-lambda/index.js
@@ -1,14 +1,9 @@
 const fetch = require('node-fetch')
 
-exports.handler = async (event, context) => {
-    const eventBody = JSON.parse(event.body)
+exports.handler = async event => {
     const response = await fetch(process.env.SLACK_MESSAGE_ENDPOINT, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(eventBody)
+        body: JSON.stringify(event)
     })
-    const responseBody = await response.json()
-    context.succeed(responseBody);
+    return response
 }
