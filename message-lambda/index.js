2b48ce6bd83d89c646befd3117d2407d99ef74f6
Ian King <ian.antking@gmail.com>
2021-01-26 13:26:57 +0000

    parse event body and stringify into json for slack api

--- message-lambda/index.js
+++ message-lambda/index.js
@@ -1,10 +1,14 @@
 const fetch = require('node-fetch')
 
 exports.handler = async (event, context) => {
+    const eventBody = JSON.parse(event.body)
     const response = await fetch(process.env.SLACK_MESSAGE_ENDPOINT, {
         method: 'POST',
-        body: event.body
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(eventBody)
     })
-    const body = await response.json()
-    context.succeed(JSON.stringify(body));
+    const responseBody = await response.json()
+    context.succeed(responseBody);
 }
