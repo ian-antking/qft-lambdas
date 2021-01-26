6e3f1f8bca3fced8cb03afc737fcc35bfc631830
Ian King <ian.antking@gmail.com>
2021-01-26 01:04:42 +0000

    update environment variable

--- message-lambda/index.js
+++ message-lambda/index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 exports.handler = async (event, context) => {
-    const response = await fetch(process.env.SLACK_ENDPOINT, {
+    const response = await fetch(process.env.SLACK_MESSAGE_ENDPOINT, {
         method: 'POST',
         body: event.body
     })
