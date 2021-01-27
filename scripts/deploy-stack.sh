5ed8cf0b70ec46d5b661e08e24c23ba880ff1ad7
Ian King <ian.antking@gmail.com>
2021-01-27 15:43:58 +0000

    add deploy-stack script

---
+++ scripts/deploy-stack.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+STACK_NAME=$1
+ENVIRONMENT=$2
+
+if [ -z "$STACK_NAME" ] || [ -z "$ENVIRONMENT" ]; then
+    echo "Usage: $(basename $0) STACK_NAME ENVIRONMENT"
+    exit 1
+fi
+
+aws cloudformation update-stack --stack-name ${ENVIRONMENT}.${STACK_NAME} --template-body file://./stacks/template.json --parameters file://./stacks/parameters/${ENVIRONMENT}.json --capabilities CAPABILITY_NAMED_IAM
