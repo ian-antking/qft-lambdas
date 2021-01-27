886425e8b3e70d9430221e884e296bf8835b36d8
Ian King <ian.antking@gmail.com>
2021-01-27 15:43:18 +0000

    delete old directory

--- message-lambda/index.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch')
-
-exports.handler = async event => {
-    const response = await fetch(process.env.SLACK_MESSAGE_ENDPOINT, {
-        method: 'POST',
-        body: JSON.stringify(event)
-    })
-    return response
-}
